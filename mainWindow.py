13b883f81330146319edbc77f5e411ce00e65884
Peter Rockett <p.rockett@sheffield.ac.uk>
2020-04-25 14:14:01 +0100

    Completed minimal example program with example dialog

--- mainWindow.py
+++ mainWindow.py
@@ -1,21 +1,25 @@
 #! /usr/bin/env python3
 
-#******************************************************************************
 # Example Qt main program -- pir -- 17.4.2020
+
+#******************************************************************************
+# Insert licence here!
+
 
 
 #******************************************************************************
 
 from sys import exit
-#from PySide2 import QtCore
-#from PySide2 import QtGui
+#from PySide2.QtCore import ???
+#from PySide2.QtGui import ???
 from PySide2.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QWidget, QSplitter
-from PySide2.QtWidgets import QMenuBar, QToolBar, QStatusBar
+from PySide2.QtWidgets import QMenuBar, QMenu, QAction
+from PySide2.QtWidgets import QToolBar
+from PySide2.QtWidgets import QStatusBar
 from PySide2.QtWidgets import QTreeWidget, QTreeWidgetItem
 from PySide2.QtWidgets import QTextEdit
 
-#******************************************************************************
-# Insert licence here!
+from exampleDialog import ExampleDialog
 
 #******************************************************************************
 
@@ -27,11 +31,19 @@ class MainWindow(QMainWindow):
         self.mainLayout = QVBoxLayout()
 
         # Create menu bar & menus
-        #TODO
-
+        self.fileMenu = self.menuBar().addMenu("&File")
+        self.openMenuAction = self.fileMenu.addAction("&Open")
+        self.openMenuAction.triggered.connect(self.on_open_action)    # New-style connect!
+        self.fileMenu.addSeparator()
+        self.quitMenuAction = self.fileMenu.addAction("&Quit")
+        self.quitMenuAction.triggered.connect(self.on_quit_action)
+        
         # Create main toolbar
-        self.mainToolBar = QToolBar(self)
+        self.mainToolBar = QToolBar()
         self.mainToolBar.setMovable(False)
+        self.openToolButton = self.mainToolBar.addAction("open")    # Replace with suitable icon      
+        self.openToolButton.triggered.connect(self.on_open_action)
+        self.addToolBar(self.mainToolBar)
         self.mainLayout.addWidget(self.mainToolBar)
 
         # Configure window splitter
@@ -59,8 +71,19 @@ class MainWindow(QMainWindow):
 
     #--------------------------------------------------------------------------
     
-    # def on_signal_handler(self):
-    #     self.text.setText(random.choice(self.hello))
+    def on_open_action(self):
+        """Handler for 'open' action"""
+        print("open a file")
+        exampleDialog = ExampleDialog("Example dialog")
+        return
+        
+   #--------------------------------------------------------------------------
+    
+    def on_quit_action(self):
+        """Handler for 'quit' action"""
+        print("quitting application")
+        self.close()  
+        return
 
 #******************************************************************************
 
@@ -71,6 +94,6 @@ if __name__ == "__main__":
     mainWindow = MainWindow()
     mainWindow.show()
 
-    exit(application.exec_())   # Not sure why this has to be `exec_` with a trailing underscore?
+    exit(application.exec_())   # Not sure why this still has to be `exec_` with a trailing underscore?
 
 #******************************************************************************

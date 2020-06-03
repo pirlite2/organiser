ef94116cebded3fc06e0490da1b53d9b5e60f4ad
ChingTung <63977132+ChingTung@users.noreply.github.com>
2020-06-03 18:38:51 +0100

    Add files via upload

---
+++ xml_R.py
@@ -0,0 +1,38 @@
+import xml.sax
+class readHandler(xml.sax.ContentHandler):
+    def __init__(self):
+        self.CurrentData = ""
+        self.title = ""
+        self.note = ""
+        self.deadline = ""
+
+    def startElement( self, type, attributes):
+        self.CurrentData =  type
+        if  type == "node":
+            print ('--- Node ---')
+            Icon = attributes["icon"]
+            print ("[", Icon, "]")
+
+    def endElement( self, type):
+        if self.CurrentData ==  "title":
+            print ( 'Title: ',  self.title)
+        elif self.CurrentData ==  "note":
+            print ( 'Note: ', self.note)
+        elif  self.CurrentData ==  "deadline":
+            print ('Deadline:',  self.deadline)
+        self.CurrentData = ""
+
+    def characters(self, content):
+        if self.CurrentData == "title":
+            self.title = content
+        elif self.CurrentData == "note":
+            self.note = content
+        elif self.CurrentData == "deadline":
+            self.deadline = content
+
+if (__name__ =="__main__"):
+    parser=xml.sax.make_parser()
+    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+    Handler =  readHandler()
+    parser.setContentHandler(Handler)
+    parser.parse("/home/chingtung/文件/organiser-master/initial-xml-sketch.xml")

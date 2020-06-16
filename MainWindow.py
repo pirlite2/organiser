8a5440283328b838ed19a26aa403ec48f0ac0181
HONG ZHOU <hzhou31@sheffield.ac.uk>
2020-06-17 04:51:34 +0800

    add new menues and icons, code to open files

--- MainWindow.py
+++ MainWindow.py
@@ -22,9 +22,13 @@ from sys import exit
 from PySide2.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QWidget, QSplitter
 from PySide2.QtWidgets import QMenuBar, QMenu, QAction
 from PySide2.QtWidgets import QToolBar, QInputDialog
-#from PySide2.QtWidgets import QStatusBar
+from PySide2.QtWidgets import QStatusBar   #Why make this line to comment?
 
 from ItemTree import *
+
+from PySide2.QtWidgets import QFileDialog
+import os
+import ItemTree
 
 #******************************************************************************
 
@@ -32,7 +36,7 @@ class MainWindow(QMainWindow):
     """
     Main application window
     :version:
-    :author: pir
+    :author: pir and Hong
     """
 
    #--------------------------------------------------------------------------
@@ -43,14 +47,28 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Organiser")
         self.mainLayout = QVBoxLayout()
 
+        self.cwd = os.getcwd()  #Gets the location of the current program file
+
         # Create menu bar & menus
         self.fileMenu = self.menuBar().addMenu("&File")
+
+        self.createNewNoteMenuAction = self.fileMenu.addAction("&Create New Note")
+        self.createNewNoteMenuAction.triggered.connect(self.on_create_new_note_action)
+
         self.openMenuAction = self.fileMenu.addAction("&Open")
         self.openMenuAction.triggered.connect(self.on_open_action)    # New-style connect!
+        
+        self.saveMenuAction = self.fileMenu.addAction("&Save")
+        self.saveMenuAction.triggered.connect(self.on_save_action)
+
+        self.saveAsMenuAction = self.fileMenu.addAction("&Save as")
+        self.saveAsMenuAction.triggered.connect(self.on_save_as_action)
         self.fileMenu.addSeparator()
+
         self.setPreferencesMenuAction = self.fileMenu.addAction("Set Preferences")
         self.setPreferencesMenuAction.triggered.connect(self.on_set_preferences_action)        
         self.fileMenu.addSeparator()
+
         self.quitMenuAction = self.fileMenu.addAction("&Quit")
         self.quitMenuAction.triggered.connect(self.on_quit_action)
         
@@ -60,11 +78,15 @@ class MainWindow(QMainWindow):
         
         self.addItemToolButton = self.mainToolBar.addAction(QIcon("./mainToolbarIcons/Gnome-item-add.svg"), "Insert new item")  # Icons from https://commons.wikimedia.org/wiki/GNOME_Desktop_icons
         self.addItemToolButton.triggered.connect(self.on_insert_item_action)      
+        
         self.addChildItemToolButton = self.mainToolBar.addAction(QIcon("./mainToolbarIcons/Gnome-item-add-child.svg"), "Insert child item")
         self.addChildItemToolButton.triggered.connect(self.on_insert_child_item_action)      
-        self.deleteItemToolButton = self.mainToolBar.addAction(QIcon("./mainToolbarIcons/Gnome-item-add.svg"), "Delete item")  
+        
+        self.deleteItemToolButton = self.mainToolBar.addAction(QIcon("./mainToolbarIcons/Gnome-item-delete.svg"), "Delete item")  
         self.deleteItemToolButton.triggered.connect(self.on_delete_item)
-        #self.editItemToolButton = self.mainToolBar.addAction(QIcon("./mainToolbarIcons/Gnome-item-add.svg"), "Edit item")
+        
+        self.editItemToolButton = self.mainToolBar.addAction(QIcon("./mainToolbarIcons/Gnome-item-edit.svg"), "Edit item")
+        self.editItemToolButton.triggered.connect(self.on_edit_item)
         
         self.mainLayout.addWidget(self.mainToolBar)
 
@@ -73,7 +95,7 @@ class MainWindow(QMainWindow):
         self.splitter.setHandleWidth(2)
 
         # Configure item tree widget
-        self.itemTree = ItemTree()
+        self.itemTree = ItemTree.ItemTree()  #find the itemtree
         self.splitter.addWidget(self.itemTree)      
         self.splitter.addWidget(self.itemTree.editBox)
         self.mainLayout.addWidget(self.splitter)
@@ -90,12 +112,21 @@ class MainWindow(QMainWindow):
         return
         
     #--------------------------------------------------------------------------
- 
+    def on_create_new_note_action(self):
+        return
+
+    def on_save_action(self):
+        return
+
+    def on_save_as_action(self):
+        return
+#----------------------------------------------------------
     def on_insert_item_action(self):
         """Handler for 'add item' action"""
 
         self.itemTree.insert_task_item(True, False)
-        
+        print("adding an item")
+
         return
         
    #--------------------------------------------------------------------------
@@ -104,7 +135,8 @@ class MainWindow(QMainWindow):
         """Handler for 'add child item' action"""
 
         self.itemTree.insert_task_item(True, True)
-           
+        print("add a child item")
+
         return
         
    #--------------------------------------------------------------------------
@@ -129,14 +161,37 @@ class MainWindow(QMainWindow):
     
     def on_open_action(self):
         """Handler for 'open' action"""
-
+        
+        fileName_choose,filetype = QFileDialog.getOpenFileName(self,
+                                    "Open",
+                                    "C:\\Users\\", # Open the file from the start path
+                                    "All Files (*);;Text Files (*.txt);;Word Files (*.docx)" )
         print("open file item") # test
 
+        if fileName_choose == "":
+            print("\nDeslect the choose")
+            return
+
+        print("\nThe file you choosed:")
+        print(fileName_choose)
+        print("File type: ",filetype)
+        
+        f = open(fileName_choose) # Read the file
+        lines = f.read()
+        #add_task_item(lines)      # add_task_item 
+        print(lines)    #test
+        f.close()
+
+        fn=fileName_choose.split("/")[-1] #Extract the file name from the address
+
+        self.itemTree.add_task_item(0, fn, lines, 0, False, 0)
+        
+        """
         # Get file name
         self.filename = "???"
-
-        # Read XML file        
         
+        # Read XML file        
+        """
         return
         
     #--------------------------------------------------------------------------

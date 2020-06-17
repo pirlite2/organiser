1bcc55d589e89654bc8c0ecc01af588b11e00d80
ChingTung <63977132+ChingTung@users.noreply.github.com>
2020-06-17 17:34:04 +0100

    xml_R.py
    'xml_R.py' allow to read a XML file and print the info.

---
+++ xml_R.py
@@ -0,0 +1,49 @@
+import xml.sax
+class readHandler(xml.sax.ContentHandler):
+    def __init__(self):
+        self.CurrentData = ""
+        self.root=""
+        self.node=""
+        self.title = ""
+        self.note = ""
+        self.deadline = ""
+        
+    def startElement( self, type, attributes):
+        self.CurrentData =  type
+        if  type == "tree":
+            print('***Tree***')
+        if type=="task":
+            Icon = attributes["icon"]
+            print ('--- Task ---')
+            print('Icon:',Icon)
+            Expanded = attributes["expanded"]
+            if Expanded=="true":
+                print('Expanded')
+            elif Expanded=="false":
+                print('Compressed')
+
+    def endElement( self, type):  
+        if self.CurrentData ==  "title":
+            print ( 'Title:',self.title)
+        elif self.CurrentData ==  "note":
+            print ( 'Note:',self.note)
+        elif  self.CurrentData ==  "deadline":
+            print ('Deadline:',self.deadline)
+        if  type == "task":
+            print ('---endtask---')
+        self.CurrentData = ""
+
+    def characters(self, content):
+        if self.CurrentData == "title":
+            self.title = content
+        elif self.CurrentData == "note":
+            self.note = content
+        elif self.CurrentData == "deadline":
+            self.deadline = content
+
+if (__name__ =="__main__"):
+    parser=xml.sax.make_parser()
+    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+    Handler =  readHandler()
+    parser.setContentHandler(Handler)
+    parser.parse("/home/chingtung/文件/organiser-development/initial-xml-sketch.xml")

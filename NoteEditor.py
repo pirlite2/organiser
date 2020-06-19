69dc130f1e3e664a0e5f03827a240499e7369d72
Peter Rockett <p.rockett@sheffield.ac.uk>
2020-06-19 17:00:35 +0100

    Sorted stash mess up

--- NoteEditor.py
+++ NoteEditor.py
@@ -1,159 +1,220 @@
-from PySide2.QtWidgets import QToolBar, QTextEdit, QAction, QFontComboBox, QComboBox, QWidget, QVBoxLayout
-from PySide2.QtGui import QFont, QTextDocument, QKeySequence, QIcon, QColor
-from PySide2.QtCore import QSize
-#from EditBox import EditBox
+#! /usr/bin/python3
+
+#******************************************************************************
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#******************************************************************************
 
 import os
 import json
 
-fontSizePickers = [8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72, 84, 96, 110, 130]
-defaultfontSize = 18.0
+from PySide2.QtCore import Qt, QSize
 
+from PySide2.QtGui import QTextDocument, QTextCursor, QTextCharFormat, QColor
+from PySide2.QtGui import QFont, QKeySequence, QIcon
 
-class NoteEditor (QWidget):
+from PySide2.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout
+from PySide2.QtWidgets import QComboBox, QFontComboBox, QToolBar, QTextEdit, QAction
+
+#******************************************************************************
+
+fontSizePickers = [8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72, 84, 96]
+defaultfontSize = 12.0
+
+#******************************************************************************
+
+class NoteEditor(QWidget):
     """
-    Container for NoteEditor instances 
-    :version: 1.0
-    :author: js
+    :version:
+    :author: pir, Joseph-William Szetu
     """
+
     def __init__(self):
         super().__init__()
 
         #initialise the widgets needed
-        self.layout = QVBoxLayout()
-        self.editBox = self.createEditBox()
-        self.toolbar = self.createToolbar()
+        widgetLayout = QVBoxLayout()
 
-        #initial text settings 
-        
+        self.noteEditBox = QTextEdit()
+        self.noteEditBox.setFontPointSize(defaultfontSize)
+        self.noteEditBox.setFont(QFont('Helvtica', defaultfontSize))
+        self.noteEditBox.textChanged.connect(self.textHasChanged)
 
-        #set up layout
-        self.layout.addWidget(self.editBox)
-        self.layout.addWidget(self.toolbar)
-        self.setLayout(self.layout)
-        return
-#---------------------------------------------------------------------------
-    def setDocument(self, document):
-        self.editBox.setDocument(document)
-        return
-#---------------------------------------------------------------------------
-    def createToolbar(self):
-        # Create main toolbar
-        mainToolBar = QToolBar()
-        mainToolBar.setMovable(False)
+        editToolbar = QToolBar()
+        editToolbar.setMovable(False)
+        editToolbar.setIconSize(QSize(18, 18))
 
-        mainToolBar.setIconSize(QSize(18, 18))
-
-        #Buttons
-        #Font Colour Picker
+        # Buttons
+        # Font Colour Picker
         self.fontColourPicker = QComboBox()
         with open(os.path.join('colours', 'colours.json')) as f:
             self.colours = json.load(f)
         self.fontColourPicker.addItems(self.colours.keys())
         self.fontColourPicker.setStatusTip("Text Colour")
-        mainToolBar.addWidget(self.fontColourPicker)
+        editToolbar.addWidget(self.fontColourPicker)
         self.fontColourPicker.setCurrentIndex(0)
         self.fontColourPicker.currentIndexChanged.connect(self.colourPickerChanged)
 
-        #Text Highlight Colour Picker
+        # Text Highlight Colour Picker
         self.textHighlight = QComboBox()
         self.textHighlight.addItems(self.colours.keys())
         self.textHighlight.setCurrentIndex(1)
         self.textHighlight.setStatusTip("Highlight Colour")
-        mainToolBar.addWidget(self.textHighlight)
+        editToolbar.addWidget(self.textHighlight)
         self.textHighlight.currentIndexChanged.connect(self.textHighlightChanged)
 
-        #Font Family
+        # Font Family
         self.fontFamilyPicker = QFontComboBox()
         self.fontFamilyPicker.setStatusTip("Font")
         self.fontFamilyPicker.currentFontChanged.connect(self.fontChanged)
-        mainToolBar.addWidget(self.fontFamilyPicker)
+        editToolbar.addWidget(self.fontFamilyPicker)
 
-        #Font Size
+        # Font Size
         self.fontSizePicker = QComboBox()
         self.fontSizePicker.addItems([str(s) for s in fontSizePickers])
         self.fontSizePicker.setCurrentIndex(fontSizePickers.index(defaultfontSize))
         self.fontSizePicker.setStatusTip("Font Size")
-        mainToolBar.addWidget(self.fontSizePicker)
+        editToolbar.addWidget(self.fontSizePicker)
         self.fontSizePicker.currentIndexChanged.connect(self.fontSizePickerChanged)
-        mainToolBar.addSeparator()
+        editToolbar.addSeparator()
 
-        #Bold
+        # Bold
         self.boldButton = QAction(QIcon(os.path.join('icons', 'bold.svg')), "Bold")
         self.boldButton.setStatusTip("Bold")
         self.boldButton.setCheckable(True)
-        self.boldButton.toggled.connect(lambda x: self.editBox.setFontWeight(QFont.Bold if x else QFont.Normal))
-        mainToolBar.addAction(self.boldButton)
+        self.boldButton.toggled.connect(lambda x: self.noteEditBox.setFontWeight(QFont.Bold if x else QFont.Normal))
+        editToolbar.addAction(self.boldButton)
 
-        #Italic
+        # Italic
         self.italicButton = QAction(QIcon(os.path.join('icons', 'italic.svg')), "Italic")
         self.italicButton.setStatusTip("Italic")
         self.italicButton.setCheckable(True)
-        self.italicButton.toggled.connect(self.editBox.setFontItalic)
-        mainToolBar.addAction(self.italicButton)
+        self.italicButton.toggled.connect(self.noteEditBox.setFontItalic)
+        editToolbar.addAction(self.italicButton)
 
-        #Underline
+        # Underline
         self.underlineButton = QAction(QIcon(os.path.join('icons', 'underline.svg')), "Underline")
         self.underlineButton.setStatusTip("Underline")
         self.underlineButton.setCheckable(True)
-        self.underlineButton.toggled.connect(self.editBox.setFontUnderline)
-        mainToolBar.addAction(self.underlineButton)
+        self.underlineButton.toggled.connect(self.noteEditBox.setFontUnderline)
+        editToolbar.addAction(self.underlineButton)
 
-        #alignment
+        widgetLayout.addWidget(editToolbar)     
+       
+        widgetLayout.addWidget(self.noteEditBox)
+        self.setLayout(widgetLayout)
+              
+        return
 
-        return(mainToolBar)
-#---------------------------------------------------------------------------    
-    def createEditBox(self):
-
-        editBox = EditBox()
-        editBox.setFontPointSize(defaultfontSize)
-        editBox.setFont(QFont('Times', defaultfontSize))
-        editBox.setPlaceholderText("New Note.....")
-        editBox.textChanged.connect(self.textHasChanged)
-
-        return(editBox)
-#---------------------------------------------------------------------------
+    #---------------------------------------------------------------------------
+    
     def textHasChanged(self):
+
         plainText = self.editBox.toPlainText()
 
         if(plainText[-1] == ' ' or plainText[-1] == '\n'):
-            print("Text changed...>>> " + self.editBox.toPlainText())
+            print("Text changed...>>> " + self.noteEditBox.toPlainText())
             """ INSERT HERE CALL TO SPELL CHECK MODULE I.E
                 self.editBox.spell_check(plainText)
             """
         return
-#---------------------------------------------------------------------------
+
+    #---------------------------------------------------------------------------
+
     def fontChanged(self):
 
         font = self.fontFamilyPicker.currentFont()
-        self.editBox.setCurrentFont(font)
+        self.noteEditBox.setCurrentFont(font)
         self.fontSizePickerChanged()
         print("font changed")
         return
-#---------------------------------------------------------------------------
+    
+    #---------------------------------------------------------------------------
+    
     def fontSizePickerChanged(self):
 
         fontSize = self.fontSizePicker.currentText()
-        self.editBox.setFontPointSize(float(fontSize))
+        self.noteEditBox.setFontPointSize(float(fontSize))
         self.fontSizePicker.setCurrentText(fontSize)
         print("font size changed")
 
         return
-#---------------------------------------------------------------------------
+
+    #---------------------------------------------------------------------------
+    
     def colourPickerChanged(self):
 
         currentColour = self.fontColourPicker.currentText()
         print("font colour changed")
         r,g,b = self.colours[currentColour]['rgb']
-        self.editBox.setTextColor(QColor(r,g,b))
+        self.noteEditBox.setTextColor(QColor(r,g,b))
 
         return
-#---------------------------------------------------------------------------
+
+    #---------------------------------------------------------------------------
+    
     def textHighlightChanged(self):
 
         currentColour = self.textHighlight.currentText()
         print("text background colour changed")
         r,g,b = self.colours[currentColour]['rgb']
-        self.editBox.setTextBackgroundColor(QColor(r,g,b))
+        self.noteEditBox.setTextBackgroundColor(QColor(r,g,b))
 
         return
+
+    #--------------------------------------------------------------------------
+
+    def spell_check(self, document):
+        """
+        Spell check document
+        :version:
+        :author: pir & ???
+        """
+
+        documentText = document.toPlainText()
+        wordList = documentText.split()
+
+        # test
+        for i in range(0, len(wordList)):
+            print(wordList[i])
+        #test
+
+        # find misspelled word & correct it
+        wrongSpelling = "folls"
+        correctSpelling = "falls"
+        currentTextCursor = QTextCursor(document)
+        textCursor = document.find(wrongSpelling, currentTextCursor, QTextDocument.FindCaseSensitively or QTextDocument.FindWholeWords)
+        print("selected word = ", textCursor.selectedText())
+
+        return
+
+#******************************************************************************
+
+if __name__ == "__main__":
+    application = QApplication([])
+
+    mainWindow = QMainWindow()
+    noteEditor = NoteEditor()
+
+    mainWindow.setCentralWidget(noteEditor)
+    mainWindow.show()
+
+    document = QTextDocument("the rain in Spaun folls maily on the plaine\n\n hello world")
+    noteEditor.setNoteDocument(document)
+    noteEditor.spell_check(document)
+
+    exit(application.exec_()) 
+
+#******************************************************************************
